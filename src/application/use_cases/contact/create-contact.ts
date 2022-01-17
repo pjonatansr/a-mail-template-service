e6e31fdac2a7176532c09edefa58da14cb2ba2a6
RayanaVital <96187743+RayanaVital@users.noreply.github.com>
2022-01-17 18:59:01 -0300

    feat(group, contact): added use cases

---
+++ src/application/use_cases/contact/create-contact.ts
@@ -0,0 +1,22 @@
+import { Contact } from '@entities/contact';
+import { IContact, IRepository, ContactOrError } from '@types';
+import { isLeft } from 'src/shared/either';
+
+const CreateContact = async (
+    contactData: IContact,
+    repository: IRepository,
+): Promise<Contact | Error> => {
+    const contactOrError: ContactOrError = Contact.create(contactData);
+
+    if (isLeft(contactOrError)) {
+        return contactOrError.value;
+    }
+
+    const contact: Contact | Error = await repository.persist(
+        contactOrError.value,
+    );
+
+    return contact;
+};
+
+export default CreateContact;
